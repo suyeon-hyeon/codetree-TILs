7129423c294fb4c2f882b926c731f6c529a232e9
suyeon-hyeon <81295902+suyeon-hyeon@users.noreply.github.com>
2023-12-13 18:46:15 +0900

    Title: 전부 겹치는 선분 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 231213/전부 겹치는 선분 2/overlapping-line-segments-2.java
+++ 231213/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -4,10 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int exceptStartIdx = -1;
-    static int exceptEndIdx = -1;
-    static int startIdx = -1;
-    static int endIdx = -1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -18,38 +14,23 @@ public class Main {
             start[i] = Integer.parseInt(st.nextToken());
             end[i] = Integer.parseInt(st.nextToken());
         }
-        if(isIntersecting(n, start, end, -1)){
-            if(isIntersecting(n,start,end, exceptStartIdx)||isIntersecting(n,start,end,exceptEndIdx))
-                System.out.println("Yes");
-            else
-                System.out.println("No");
+        boolean isIntersecting = false;
+        for (int i=0;i<n;i++){
+            int maxStart = 0;
+            int minEnd = Integer.MAX_VALUE;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                maxStart=Math.max(maxStart,start[j]);
+                minEnd=Math.min(minEnd,end[j]);
+            }
+            if(minEnd>=maxStart){
+                isIntersecting = true;
+                break;
+            }
         }
-        else
+        if(isIntersecting)
             System.out.println("Yes");
-    }
-    // 겹치는 여부 확인 함수
-    static boolean isIntersecting(int n, int[] start, int[] end, int exceptIdx){
-        int maxStart = 0;
-        int minEnd = Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            if(i==exceptIdx)
-                continue;
-            if(maxStart<start[i]){
-                maxStart=start[i];
-                startIdx=i;
-            }
-            if(minEnd>end[i]){
-                minEnd=end[i];
-                endIdx=i;
-            }
-        }
-        if(minEnd<maxStart){
-            if(exceptIdx==exceptStartIdx)
-                exceptStartIdx = startIdx;
-            if(exceptIdx==exceptEndIdx)
-                exceptEndIdx = endIdx;
-            return false;
-        } else
-            return true;
+        else
+            System.out.println("No");
     }
 }
