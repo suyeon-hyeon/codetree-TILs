bfa789f9f5e70f01669a72b77cb0508625638b8f
suyeon-hyeon <81295902+suyeon-hyeon@users.noreply.github.com>
2023-12-13 16:55:42 +0900

    Title: 구역 청소, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231213/구역 청소/district-cleaning.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        if (b < c || d < a) {
+            System.out.println((b - a) + (d - c));
+        } else {
+            System.out.println(Math.max(b,d)-Math.min(a,c));
+        }
+    }
+}
