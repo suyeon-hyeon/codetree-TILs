6b3db270875e7e8b99cb53d81a6af5d9a8eb6cf4
suyeon-hyeon <81295902+suyeon-hyeon@users.noreply.github.com>
2024-01-02 00:28:50 +0900

    Title: 독서실의 거리두기 2, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240102/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 240102/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -7,30 +7,44 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		char[] in = br.readLine().toCharArray();
-		int pre = -1, cur = 0;
+		int pre = -1, cur = -1, start = -1;
 		int idx = -1, max = 0;
-		for(int i=1;i<n;i++) {
-			if(in[i]=='1') {
+		for (int i = 0; i < n; i++) {
+			if (in[i] == '1') {
+				if (cur == -1) {
+					cur = i;
+					start = i;
+					continue;
+				}
 				pre = cur;
 				cur = i;
-				if(max<cur-pre) {
-					max = cur-pre;
-					idx = (cur+pre)/2;
+				if (max < cur - pre) {
+					max = cur - pre;
+					idx = (cur + pre) / 2;
 				}
 			}
 		}
-		if(cur!=n-1 && max/2<n-1-cur) {
-			idx = n-1;
+		max /= 2;
+		if (start != 0 && max < start) {
+			max = start;
+			idx = 0;
+		}
+		if (cur != n - 1 && max < n - 1 - cur) {
+			idx = n - 1;
 		}
 		in[idx] = '1';
 		pre = -1;
-		cur = 0;
+		cur = -1;
 		int min = Integer.MAX_VALUE;
-		for(int i=1;i<n;i++) {
-			if(in[i]=='1') {
+		for (int i = 0; i < n; i++) {
+			if (in[i] == '1') {
+				if (cur == -1) {
+					cur = i;
+					continue;
+				}
 				pre = cur;
 				cur = i;
-				min = Math.min(min, cur-pre);
+				min = Math.min(min, cur - pre);
 			}
 		}
 		System.out.println(min);
