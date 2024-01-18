106b0a057a87b7f7352e2e3036d156b354c7d436
suyeon-hyeon <81295902+suyeon-hyeon@users.noreply.github.com>
2024-01-18 17:37:26 +0900

    Title: 홀수 짝수의 묶음, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240118/홀수 짝수의 묶음/odd-even-bundle.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int odd = 0, even = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num % 2 == 1)
+                odd++;
+            else
+                even++;
+        }
+        int cnt = 0;
+        if (even > odd) {
+            cnt = odd * 2 + 1;
+        } else if (odd == even) {
+            cnt = odd * 2;
+        } else {
+            cnt = even * 2 + (odd-even)/3*2;
+        }
+        System.out.println(cnt);
+    }
+}
