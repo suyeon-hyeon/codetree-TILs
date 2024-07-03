416477be2358d0a6f5c0e443a7f942d9e58be837
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2024-07-03 18:48:47 +0900

    Title: 거품 정렬 구현, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240703/거품 정렬 구현/implement-bubble-sort.java
+++ 240703/거품 정렬 구현/implement-bubble-sort.java
@@ -12,19 +12,17 @@ public class Main {
         for(int i=0;i<n;i++)
             arr[i] = Integer.parseInt(st.nextToken());
         boolean sorted;
-        for(int i=0;i<n-1;i++){
+        do {
             sorted = true;
-            for(int j=0;j<n-1;j++) {
-                if(arr[j]>arr[j+1]) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                     sorted = false;
                 }
             }
-            if(sorted)
-                break;
-        }
+        } while (!sorted);
         for(int i=0;i<n;i++)
             System.out.print(arr[i]+" ");
     }
