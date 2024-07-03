0fc52a49659d13666985917dae04fd03b9336d44
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2024-07-03 17:11:18 +0900

    Title: 황금비율 토스트, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240703/황금비율 토스트/golden-toast.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 식빵 개수 n과 레시피 암호문 개수 m
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        // 식빵 알파벳
+        String[] alpha = br.readLine().split("");
+        LinkedList<String> list = new LinkedList<>(Arrays.asList(alpha).subList(0, n));
+        // 레시피
+        ListIterator<String> it = list.listIterator(list.size());
+        for(int i=0;i<m;i++) {
+            st = new StringTokenizer(br.readLine());
+            char recipe = st.nextToken().charAt(0);
+            switch (recipe) {
+                case 'L':
+                    if(it.hasPrevious())
+                        it.previous();
+                    break;
+                case 'R':
+                    if(it.hasNext())
+                        it.next();
+                    break;
+                case 'D':
+                    if(it.hasNext()){
+                        it.next();
+                        it.remove();
+                    }
+                    break;
+                case 'P':
+                    it.add(st.nextToken());
+            }
+        }
+        // 출력
+        for(int i=0;i<n;i++)
+            System.out.print(list.pollFirst());
+    }
+}
