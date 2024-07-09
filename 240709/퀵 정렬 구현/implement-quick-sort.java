9383692887b9ee2f76d4dc65c9ca481ae133ca3e
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2024-07-09 16:57:31 +0900

    Title: 퀵 정렬 구현, Time: 1521ms, Memory: 51MB, Status: Passed - CodeTree

--- 240709/퀵 정렬 구현/implement-quick-sort.java
+++ 240709/퀵 정렬 구현/implement-quick-sort.java
@@ -1,77 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int[] sorted;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // n개의 원소
-        n = Integer.parseInt(br.readLine());
-        // 정렬될 배열
-        sorted = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++)
-            sorted[i] = Integer.parseInt(st.nextToken());
-        // 퀵정렬 수행
-        quick_sort(sorted,0,n-1);
-        // 정렬된 결과물 출력
-        for(int i=0;i<n;i++)
-            System.out.print(sorted[i]+" ");
-    }
+    public static final int MAX_N = 100000;
+    
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static int[] mergedArr = new int[MAX_N];
 
-    // 퀵정렬 함수
-    static void quick_sort(int[] arr, int low, int high) {
-        // 재귀 조건
-        if(low<high){
-            // 두 그룹으로 분류
-            int pos = partition(arr, low, high);
-
-            // pivot 기준으로 분류된 두 그룹을 다시 퀵정렬
-            quick_sort(arr, low, pos-1);
-            quick_sort(arr, pos+1, high);
-        }
-    }
-
-    // pivot 기준으로 분류해주는 함수
-    static int partition(int[] arr, int low, int high) {
-        int pivot = select_pivot(arr, low, high);
-        int i = low-1;
-        // pivot보다 작을 경우 swap
-        for(int j=low;j<=high-1;j++) {
-            if(arr[j]<pivot){
-                i++;
-                swap(arr,i,j);
+    public static int partition(int low, int high) {
+        int pivot = arr[high];              // pivot을 고릅니다.
+        int i = low - 1;                    // 파란색 화살표 위치를 정해줍니다.
+                                            // 파란색 화살표는 pivot보다 같거나 큰 
+                                            // 원소를 가리키고 있습니다.
+        for(int j = low; j < high; j++)     // 빨간색 화살표를 움직입니다.
+            if(arr[j] < pivot) {            // 빨간색 화살표가 가리키는 원소가
+                i++;                        // pivot보다 작다면, 왼쪽으로 가야하므로
+                int temp = arr[i];          // 두 원소의 위치를 바꿔줍니다.
+                arr[i] = arr[j];
+                arr[j] = temp;       
             }
+        
+        int temp = arr[i + 1];              // 최종적으로 pivot을
+        arr[i + 1] = arr[high];             // 경계에 있는 원소와 교환해줍니다.
+        arr[high] = temp;                   // 이때 경계는 마지막 파란색 화살표 위치에
+                                            // 1을 더한 위치입니다.
+        return i + 1;                       // pivot의 최종 위치를 반환해줍니다.
+    }
+
+    public static void quickSort(int low, int high) {
+        if(low < high) {                    // 원소의 개수가 2개 이상인 경우에만 진행
+            int pos = partition(low, high); // pivot 기준으로 좌우로 분할 한 후
+                                            // 해당 pivot의 위치를 pos에 넣어줍니다.
+            quickSort(low, pos - 1);        // pivot의 왼쪽 구간에 있는 원소들을 정렬합니다. 
+            quickSort(pos + 1, high);       // pivot의 오른쪽 구간에 있는 원소들을 정렬합니다.
         }
-        // 한바퀴 다 돈 후에 pivot과 swap
-        swap(arr,i+1,high);
-        return i+1;
     }
 
-    // pivot 선택 함수
-    static int select_pivot(int[] arr, int low, int high) {
-        if(arr.length<=3)
-            return arr[high];
-        // 중간값과 pivot값
-        int mid = (low+high)/2;
-        int pivot_idx;
-        // 만일 맨 오른쪽이 pivot일 경우 swap 없이 바로 return
-        if(arr[mid]<=arr[high]&&arr[high]<=arr[low])
-            return arr[high];
-        // 그 이외의 경우 pivot과 high swap 후 high값 return
-        if(arr[low]<=arr[mid]&&arr[mid]<=arr[high])
-            pivot_idx = mid;
-        else
-            pivot_idx = low;
-        swap(arr, pivot_idx, high);
-        return arr[high];
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        quickSort(0, n - 1);
 
-    // swap 함수
-    static void swap (int[] arr, int idx_a, int idx_b) {
-        int tmp = arr[idx_a];
-        arr[idx_a] = arr[idx_b];
-        arr[idx_b] = tmp;
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
 }
