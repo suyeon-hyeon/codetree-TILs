0fbddf5652220c17312ffe31a5071dd9e54088c7
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2024-07-10 10:52:42 +0900

    Title: 힙 정렬 구현, Time: 813ms, Memory: 25MB, Status: Passed - CodeTree

--- 240710/힙 정렬 구현/implement-heap-sort.java
+++ 240710/힙 정렬 구현/implement-heap-sort.java
@@ -9,27 +9,27 @@ public class Main {
         // n개의 원소
         n = Integer.parseInt(br.readLine());
         // 정렬될 배열
-        sorted = new int[n];
+        sorted = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
             sorted[i] = Integer.parseInt(st.nextToken());
         // 힙정렬 수행
         heap_sort(sorted,n);
         // 정렬된 결과물 출력
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
             System.out.print(sorted[i]+" ");
     }
 
     // 힙정렬 함수
     static void heap_sort(int[] arr, int n) {
         // n/2부터 1까지 heapify 수행
-        for(int i=n/2-1;i>=0;i--)
+        for(int i=n/2;i>=1;i--)
             heapify(arr, n, i);
 
         // 최댓값을 정렬 후 heapify(1) 수행
-        for(int i=n-1;i>=0;i--) {
-            swap(arr, 0, i);
-            heapify(arr, i-1, 0);
+        for(int i=n;i>1;i--) {
+            swap(arr, 1, i);
+            heapify(arr, i-1, 1);
         }
     }
 
@@ -42,9 +42,9 @@ public class Main {
         int r = i*2+1;
 
         // 가장 큰 값 판별
-        if(l<=n-1 && arr[l]>arr[largest])
+        if(l<=n && arr[l]>arr[largest])
             largest = l;
-        if(r<=n-1 && arr[r]>arr[largest])
+        if(r<=n && arr[r]>arr[largest])
             largest = r;
 
         // 가장 큰 값이 본인이 아닐 경우 swap 후 재귀
