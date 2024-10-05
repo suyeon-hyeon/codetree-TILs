12edbd0718fb3711af9393d22c0c95933ebbebb8
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2024-10-05 16:53:41 +0900

    Title: 행복한 수열의 개수, Time: 106ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241005/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 변수 선언
+    public static int n;
+    public static int m;
+    public static int[][] grid;
+
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        grid = new int[n][n];
+        int ans = 0;
+
+        // 입력 + 행 로직
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            // 연속인 수 개수
+            int cnt = 1;
+            for(int j=0;j<n;j++){
+                // 입력
+                grid[i][j] = Integer.parseInt(st.nextToken());
+
+                // 연속인 경우 count
+                if(j>0 && grid[i][j]==grid[i][j-1])
+                    cnt++;
+                else
+                    cnt = 1;
+            }
+
+            // count한 수가 m보다 클 경우 ans 증가
+            if(cnt>=m)
+              ans++;
+        }
+
+        // 열 로직
+        for(int i=0;i<n;i++) {
+            // 연속인 수 개수
+            int cnt = 0;
+            for(int j=1;j<n;j++) {
+                // 연속인 경우 count
+                if(grid[j][i]==grid[j-1][i])
+                    cnt++;
+                else
+                    cnt = 1;
+            }
+
+            // count한 수가 m보다 클 경우 ans 증가
+            if(cnt>=m)
+                ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
