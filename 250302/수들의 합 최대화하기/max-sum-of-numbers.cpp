60b2a6c067b37ec9e16e4d965256076c5721c68d
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2025-03-02 17:30:50 +0900

    Title: 수들의 합 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250302/수들의 합 최대화하기/max-sum-of-numbers.cpp
+++ 250302/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -7,30 +7,25 @@ using namespace std;
 #define MAX_N 10
 
 int n;
-bool visited_x[MAX_N+1];
-bool visited_y[MAX_N+1];
+bool visited[MAX_N+1];
 int grid[MAX_N+1][MAX_N+1];
 int max_result;
 
-void Choose(int cur_num, int result) {
-    if(cur_num == n+1) {
+void Choose(int row, int result) {
+    if(row == n+1) {
         max_result=max(max_result,result);
         return;
     }
 
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++) {
-            if(visited_x[i]||visited_y[j])
+        if(visited[i])
             continue;
             
-            visited_x[i]=true;
-            visited_y[j]=true;
+        visited[i]=true;
 
-            Choose(cur_num+1,result+grid[i][j]);
+        Choose(row+1,result+grid[row][i]);
 
-            visited_x[i]=false;
-            visited_y[j]=false;
-        }
+        visited[i]=false;
     }
 
 }
