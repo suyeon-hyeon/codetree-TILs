1d082147ffcb2eade2b21baff8e3513e8cc8255e
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2025-03-16 04:47:16 +0900

    Title: 사각형 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250316/사각형 채우기 2/rectangle-fill-2.cpp
+++ 250316/사각형 채우기 2/rectangle-fill-2.cpp
@@ -1,41 +1,24 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-#define MAX_N 100
+#define MAX_N 1000
 
 int n;
-int grid[MAX_N][MAX_N];
-long dp[MAX_N][MAX_N];
+long dp[MAX_N+1];
 
 int main() {
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
+    dp[0]=1;
+    dp[1]=1;
+
+    for(int i=2;i<=n;i++) {
+        dp[i]=dp[i-1]+dp[i-2]*2;
+        dp[i]%=10007;
     }
 
-    dp[0][0]=grid[0][0];
-    for(int i=1;i<n;i++){
-        dp[0][i]=grid[0][i]+dp[0][i-1];
-        dp[i][0]=grid[i][0]+dp[i-1][0];
-    }
-
-    for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
-            dp[i][j]=grid[i][j]+max(dp[i-1][j],dp[i][j-1]);
-        }
-    }
-
-    long ans=dp[n-1][n-1];
-    for(int i=0;i<n-1;i++){
-        ans=max(ans,dp[n-1][i]);
-        ans=max(ans,dp[i][n-1]);
-    }
-    cout<<ans;
+    cout<<dp[n];
 
     return 0;
 }
