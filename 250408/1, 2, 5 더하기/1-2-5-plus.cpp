e8491c9ebce72ce36c8d17dce8aae95600d3f2a1
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2025-04-08 06:52:35 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250408/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n;
+int num[3] = {1,2,5};
+int dp[1001];
+
+int main() {
+    cin >> n;
+
+    for(int i=1;i<=n;i++)
+        dp[i]=0;
+    dp[0]=1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<3;j++){
+            if(i>=num[j]){
+                dp[i]+=dp[i-num[j]];
+            }
+        }
+    }
+
+    cout<<dp[n]%10007;
+
+    return 0;
+}
