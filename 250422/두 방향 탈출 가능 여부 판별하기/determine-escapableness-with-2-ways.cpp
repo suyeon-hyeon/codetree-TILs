6c4f1528c3ad5ac631eabc00ee31f2c64b13518f
현수연 <81295902+suyeon-hyeon@users.noreply.github.com>
2025-04-22 00:47:41 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250422/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_NUM 100
+
+using namespace std;
+
+int n, m;
+int ans;
+
+int graph[MAX_NUM + 1][MAX_NUM + 1];
+
+bool CanGo(int x, int y) {
+    return x>=0&&x<n&&y>=0&&y<n&&graph[x][y]==1;
+}
+
+void DFS(int x, int y) {
+    if(x==n-1&&y==n-1){
+        ans=1;
+        return;
+    }
+    int dx[2] = {0,1};
+    int dy[2] = {1,0};
+
+    for(int i = 0; i < 2; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(CanGo(nx,ny)){
+            DFS(nx,ny);
+        }
+    }
+}
+
+int main() { 
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin>>graph[i][j];
+        }
+    }
+
+    ans=0;
+
+    DFS(0,0);
+
+    cout<<ans;
+}
